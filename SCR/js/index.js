c5d68022714301620a1a12c9106cc8fd329df483
GcmGabriel <127634732+GcmGabriel@users.noreply.github.com>
2023-03-11 22:53:21 -0300

    Add files via upload

---
+++ SCR/js/index.js
@@ -0,0 +1,28 @@
+const botoesCarrosel = document.querySelectorAll('.botao')
+const imagens = document.querySelectorAll('.imagem')
+
+botoesCarrosel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        desativarBotaoSelecionado()
+
+        botao.classList.add('selecionado')
+
+        esconderImagemAtiva()
+
+        imagens[indice].classList.add('ativa')
+    }) 
+})
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa')
+    imagemAtiva.classList.remove('ativa')
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado')
+    botaoSelecionado.classList.remove('selecionado')
+}
+
+
+
